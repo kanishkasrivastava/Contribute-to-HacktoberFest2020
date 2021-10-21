a4ecefa573e20b73868e85dad461127d3a3517e2
kanishkasrivastava <srivastavadev171@gmail.com>
2021-10-21 16:49:32 +0000

    added program for hacktoberfest 2021

---
+++ Projects/C++/find_remainder.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,rem;
+	    cin>>a>>b;
+	    rem=a%b;
+	    cout<<rem<<endl;
+	}
+	return 0;
+}
